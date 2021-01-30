fdea9b3b42fabe5a88b457a505ce87d184038a0d
William Vincent <compelledtograce@gmail.com>
2021-01-30 02:29:14 -0500

    added button for adding a folder and styled new folder form

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -3,11 +3,17 @@ import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDumpsterFire, faPenSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDumpsterFire,
+  faPenSquare,
+  faFolderPlus,
+  faFileCode,
+} from "@fortawesome/free-solid-svg-icons";
 
 import CommandPrompt from "../CommandPrompt";
 import TextEditor from "../TextEditor";
 import EditFolderForm from "../Forms/EditFolderForm";
+import NewFolderForm from "../Forms/NewFolderForm";
 import * as UserActions from "../../store/reducers/userInfo";
 import * as SessionActions from "../../store/reducers/session";
 
@@ -38,9 +44,25 @@ const HomePage = () => {
   return (
     <div className="bg-main bg-cover h-screen grid grid-cols-7">
       <div className="col-start-1 col-end-7 grid grid-rows-9">
-        <div className="row-start-1 row-end-2 pt-8 pl-7">
+        <div className="row-start-1 row-end-2 pt-8 pl-3 flex justify-between ml-10">
           <CommandPrompt location={location} setLocation={setLocation} />
+          <div className="mr-10 pr-2">
+            <button className=" text-accentTwo text-3xl pr-3">
+              <Link to="/home">
+                <FontAwesomeIcon icon={faFileCode} />
+              </Link>
+            </button>
+            <button
+              className=" text-accentTwo text-3xl"
+              onClick={() => setLocation(window.location.pathname)}
+            >
+              <Link to="/home/folder/new">
+                <FontAwesomeIcon icon={faFolderPlus} />
+              </Link>
+            </button>
+          </div>
         </div>
+
         <div className=" flex justify-center items-center row-start-2 row-span-full bg-secondTransparent2 pl-5 mr-10 ml-10 mb-10 shadow-md ">
           <Switch>
             <Route exact path="/home">
@@ -50,6 +72,9 @@ const HomePage = () => {
               <div>
                 <EditFolderForm />
               </div>
+            </Route>
+            <Route path="/home/folder/new">
+              <NewFolderForm />
             </Route>
           </Switch>
         </div>
